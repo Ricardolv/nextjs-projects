91947c7d9d99e994848ca803711b8ecab3594ab0
Ricardo Viana <richardluizv@gmail.com>
2022-01-25 23:46:45 -0300

    implementando component titulo

---
+++ admin-template/src/pages/notificacoes.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/template/Layout";
+
+export default function Notificacoes() {
+    return (
+        <Layout titulo="Notificacoes"
+        subtitulo="Aqui voce ira gerenciar as suas notificacoes!">
+            <h3>Conteudo!!!</h3>
+        </Layout>
+    )
+}
