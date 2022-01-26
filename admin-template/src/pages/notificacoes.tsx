8610f87a6cc0b25fd2c6b9c29027f85b812d0b02
Ricardo Viana <richardluizv@gmail.com>
2022-01-26 10:41:01 -0300

    implementando tela de autenticacao

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -1,15 +1,11 @@
 import Layout from "../components/template/Layout";
-import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
-    const  {alternarTema} = useAppData()
 
     return (
         <Layout titulo="Notificacoes"
         subtitulo="Aqui voce ira gerenciar as suas notificacoes!">
             <h3>Conteudo!!!</h3>
-            
-            <button onClick={alternarTema}>Click</button>
         </Layout>
     )
 }
