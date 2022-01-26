c3fa3437eb325c7f9f9ed632aef431c399b6242c
Ricardo Viana <richardluizv@gmail.com>
2022-01-26 09:41:09 -0300

    implementando context api

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -1,10 +1,15 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
+
+    const dados = useAppData()
+
     return (
         <Layout titulo="Notificacoes"
         subtitulo="Aqui voce ira gerenciar as suas notificacoes!">
             <h3>Conteudo!!!</h3>
+            <h3>{dados.nome}</h3>
         </Layout>
     )
 }
